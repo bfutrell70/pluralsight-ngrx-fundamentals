5b01dc5e159c7865cb5988eb07dc6c53e8781735
Brian Futrell <bfutrel@gmail.com>
2026-03-25 11:54:26 -0400

    Module 7 - RxJs Mapping Operators and Effects

--- demo-0/src/app/products/state/products.effects.ts
+++ demo-0/src/app/products/state/products.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { ProductsService } from "../products.service";
-import { catchError, concatMap, map, of } from "rxjs";
+import { catchError, concatMap, exhaustMap, map, mergeMap, of } from "rxjs";
 import { ProductsAPIActions, ProductsPageActions } from "./products.actions";
 
 @Injectable()
@@ -13,10 +13,58 @@ export class ProductEffects {
   loadProducts$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ProductsPageActions.loadProducts),
-      concatMap(() => this.productsService.getAll().pipe(
+      exhaustMap(() => this.productsService.getAll().pipe(
         map((products) => ProductsAPIActions.productsLoadedSuccess({ products })),
         catchError(
           (error) => of(ProductsAPIActions.productsLoadedFail({ message: error}))
+        )
+      ))
+    )
+  )
+
+  addProducts$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.addProduct),
+      mergeMap(({product}) => this.productsService.add(product).pipe(
+        map((newProduct) => ProductsAPIActions.productAddedSuccess({ product: newProduct })),
+        catchError(
+          (error) => of(ProductsAPIActions.productAddedFail({ message: error}))
+        )
+      ))
+    )
+  )
+
+  addProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.addProduct),
+      mergeMap(({product}) => this.productsService.add(product).pipe(
+        map((newProduct) => ProductsAPIActions.productAddedSuccess({ product: newProduct })),
+        catchError(
+          (error) => of(ProductsAPIActions.productAddedFail({ message: error}))
+        )
+      ))
+    )
+  )
+
+  updateProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.updateProduct),
+      concatMap(({product}) => this.productsService.update(product).pipe(
+        map((newProduct) => ProductsAPIActions.productUpdatedSuccess({ product: newProduct })),
+        catchError(
+          (error) => of(ProductsAPIActions.productUpdatedFail({ message: error}))
+        )
+      ))
+    )
+  )
+
+  deleteProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.deleteProduct),
+      mergeMap(({id}) => this.productsService.delete(id).pipe(
+        map((newProduct) => ProductsAPIActions.productDeletedSuccess({ id })),
+        catchError(
+          (error) => of(ProductsAPIActions.productDeletedFail({ message: error}))
         )
       ))
     )
--- demo-0/src/app/products/state/products.reducer.ts
+++ demo-0/src/app/products/state/products.reducer.ts
@@ -26,7 +26,6 @@ export const productsReducer = createReducer(
     ...state,
     loading: true,
     products: [],
-
   })),
   // added this to get displaying the products list to work
   on(ProductsAPIActions.productsLoadedSuccess, (state, { products }) => ({
@@ -39,5 +38,60 @@ export const productsReducer = createReducer(
     ...state,
     errorMessage: message,
     loading: false,
+  })),
+
+
+  on(ProductsPageActions.addProduct, (state) => ({
+    ...state,
+    loading: true,
+    errorMessage: '',
+  })),
+  on(ProductsAPIActions.productAddedSuccess, (state, {product}) => ({
+    ...state,
+    loading: false,
+    products: [...state.products, product],
+  })),
+  on(ProductsAPIActions.productAddedFail, (state, {message}) => ({
+    ...state,
+    loading: false,
+    errorMessage: message,
+  })),
+
+
+  on(ProductsPageActions.updateProduct, (state) => ({
+    ...state,
+    loading: true,
+    errorMessage: '',
+  })),
+  on(ProductsAPIActions.productUpdatedSuccess, (state, {product}) => ({
+    ...state,
+    loading: false,
+    products: state.products.map((existingProduct) =>
+      existingProduct.id === product.id ? product: existingProduct
+    ),
+  })),
+  on(ProductsAPIActions.productUpdatedFail, (state, {message}) => ({
+    ...state,
+    loading: false,
+    errorMessage: message,
+  })),
+
+
+  on(ProductsPageActions.deleteProduct, (state) => ({
+    ...state,
+    loading: true,
+    errorMessage: '',
+  })),
+  on(ProductsAPIActions.productDeletedSuccess, (state, {id}) => ({
+    ...state,
+    loading: false,
+    products: state.products.filter((existingProduct) =>
+      existingProduct.id !== id
+    ),
+  })),
+  on(ProductsAPIActions.productDeletedFail, (state, {message}) => ({
+    ...state,
+    loading: false,
+    errorMessage: message,
   })),
 );
