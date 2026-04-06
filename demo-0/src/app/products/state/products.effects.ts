a41620b53752b279f704a547319dd8fda4ccf597
Brian Futrell <bfutrel@gmail.com>
2026-04-06 07:29:23 -0400

    fixed issues causing editing a product to not reload the product list

--- demo-0/src/app/products/state/products.effects.ts
+++ demo-0/src/app/products/state/products.effects.ts
@@ -15,55 +15,54 @@ export class ProductEffects {
   loadProducts$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ProductsPageActions.loadProducts),
-      exhaustMap(() => this.productsService.getAll().pipe(
-        map((products) => ProductsAPIActions.productsLoadedSuccess({ products })),
-        catchError(
-          (error) => of(ProductsAPIActions.productsLoadedFail({ message: error}))
+      exhaustMap(() => 
+        this.productsService.getAll().pipe(
+          map((products) => 
+            ProductsAPIActions.productsLoadedSuccess({ products })
+          ),
+          catchError(
+            (error) => of(ProductsAPIActions.productsLoadedFail({ message: error}))
+          )
         )
-      ))
+      )
     )
-  )
+  );
 
   addProducts$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ProductsPageActions.addProduct),
-      mergeMap(({product}) => this.productsService.add(product).pipe(
-        map((newProduct) => ProductsAPIActions.productAddedSuccess({ product: newProduct })),
-        catchError(
-          (error) => of(ProductsAPIActions.productAddedFail({ message: error}))
+      concatMap(({product}) => 
+        this.productsService.add(product).pipe(
+          map((newProduct) => 
+            ProductsAPIActions.productAddedSuccess({ product: newProduct })
+          ),
+          catchError((error) => 
+            of(ProductsAPIActions.productAddedFail({ message: error}))
+          )
         )
-      ))
+      )
     )
-  )
-
-  addProduct$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(ProductsPageActions.addProduct),
-      mergeMap(({product}) => this.productsService.add(product).pipe(
-        map((newProduct) => ProductsAPIActions.productAddedSuccess({ product: newProduct })),
-        catchError(
-          (error) => of(ProductsAPIActions.productAddedFail({ message: error}))
-        )
-      ))
-    )
-  )
+  );
 
   updateProduct$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ProductsPageActions.updateProduct),
-      concatMap(({product}) => this.productsService.update(product).pipe(
-        map((newProduct) => ProductsAPIActions.productUpdatedSuccess({ product: newProduct })),
-        catchError(
-          (error) => of(ProductsAPIActions.productUpdatedFail({ message: error}))
+      concatMap(({product}) => 
+        this.productsService.update(product).pipe(
+          map(() => ProductsAPIActions.productUpdatedSuccess({ product })),
+          catchError(
+            (error) => of(ProductsAPIActions.productUpdatedFail({ message: error}))
+          )
         )
-      ))
+      )
     )
   )
 
   deleteProduct$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ProductsPageActions.deleteProduct),
-      mergeMap(({id}) => this.productsService.delete(id).pipe(
+      mergeMap(({id}) => 
+        this.productsService.delete(id).pipe(
         map((newProduct) => ProductsAPIActions.productDeletedSuccess({ id })),
         catchError(
           (error) => of(ProductsAPIActions.productDeletedFail({ message: error}))
